550736e21832fd1bfc1c442525d795155e896d74
Paul Patterson <paul@pattersonmultimedia.com>
2024-05-20 20:11:42 -0500

    First Commit

---
+++ USSCerritosHelpDesk/Models/USSCerritosHelpdesk.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace USSCerritosHelpDesk.Models;
+
+public class User
+{
+	public int Id { get; set; }
+	[Required]
+    public int DepartmentId { get; set; }
+	[Required]
+	public string DepartmentName { get; set; }
+    public string FirstName { get; set; }
+	public string LastName { get; set; }
+	public string CommBageId { get; set; }
+	public string Role { get; set; }
+		
+}
+
+
